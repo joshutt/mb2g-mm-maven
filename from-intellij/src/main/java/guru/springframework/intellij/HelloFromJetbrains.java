65c7618e6643828db5eddf549ea89f42dc03589c
Josh <3430+joshutt@users.noreply.github.com>
2021-01-23 18:52:51 -0500

    Resync minor changes after assignment

--- from-intellij/src/main/java/guru/springframework/intellij/HelloFromJetbrains.java
+++ from-intellij/src/main/java/guru/springframework/intellij/HelloFromJetbrains.java
@@ -3,6 +3,6 @@ package guru.springframework.intellij;
 public class HelloFromJetbrains {
 
     public String sayHello() {
-        return "Hello from JetBrains";
+        return "Hello from JetBrains!";
     }
 }

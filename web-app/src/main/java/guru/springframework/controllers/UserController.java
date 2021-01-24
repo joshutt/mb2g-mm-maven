8efc52cc5c62d1ec115cbfd68e41cc1d6e5cf78c
Josh <3430+joshutt@users.noreply.github.com>
2021-01-23 19:29:50 -0500

    Minor adjustments to account for assignment results

--- web-app/src/main/java/guru/springframework/controllers/UserController.java
+++ web-app/src/main/java/guru/springframework/controllers/UserController.java
@@ -1,12 +1,14 @@
 package guru.springframework.controllers;
 
+import guru.springframework.converters.UserMapper;
 import guru.springframework.domain.UserCommand;
 import guru.springframework.entities.User;
 
 public class UserController {
 
     User saveUser(UserCommand command) {
+
         // fake impl
-        return new User();
+        return UserMapper.INSTANCE.userCommandToUser(command);
     }
 }
